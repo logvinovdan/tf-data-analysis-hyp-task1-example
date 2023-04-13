0e3742fa3e17613fafac6dae94ece84a89144507
logvinovdan <76569554+logvinovdan@users.noreply.github.com>
2023-04-13 23:29:19 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,15 +2,17 @@ import pandas as pd
 import numpy as np
 from statsmodels.stats.proportion import proportions_ztest
 
-
 chat_id = 834639322 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
+    # Измените код этой функции
+    # Это будет вашим решением
+    # Не меняйте название функции и её аргументы
     alpha = 0.02
-    count = [x_success, y_success]
-    nobs = [x_cnt, y_cnt]
-    _, p_val = proportions_ztest(count, nobs, alternative='larger')
-    return p_val < alpha
+    stat, pval = proportions_ztest(count=[x_success, y_success], 
+                                   nobs=[x_cnt, y_cnt], alternative='larger')
+    result = pval < alpha
+    return result
